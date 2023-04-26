c65aa18ba9ee80eff73088e8ac99acf112c79f75
SONGJIHOON213213 <126453984+SONGJIHOON213213@users.noreply.github.com>
2023-04-26 09:36:36 +0900

    Update Practice6.py

---
+++ Practice6.py
@@ -0,0 +1,10 @@
+number = 0 
+for mark in marks: 
+    number = number + 1
+    if mark < 60: continue
+    print("%d번 학생 축하합니다." % number)
+test_list = ['owm','two','three']
+for i in test_list:
+    print(i)
+while True:
+    print("Ctrl+C를 눌러야 while문을빠져나간다")
